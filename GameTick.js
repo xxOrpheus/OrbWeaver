586bfe14b8922c5a8667582653944f9b2d5a5330
dave <lolidunno@live.co.uk>
2023-09-01 01:31:00 -0700

    woops

--- GameTick.js
+++ GameTick.js
@@ -23,11 +23,9 @@ class GameTick {
 						const updateUserId = this.updateQueue[updateType][updateUser].id;
 						const regionUsers = this.server.users.regionMap;
 						const updateUserObj = this.server.users.users[updateUserId];
+						// we only need to update users in the same region
 						for (const receiveUpdateUser in regionUsers) {
-							// we only need to update users in groups
 							const receiveUpdateUserObj = this.server.users.users[receiveUpdateUser];
-							const receiveUpdateLocation = receiveUpdateUserObj.location;
-							const updateUserLocation = updateUserObj.location;
 							if (updateUserObj.id != receiveUpdateUserObj.id) {
 								// TODO: if a new player enters the region, we will need to request updates for all players.. hmm...
 								this.server.groups.sendPlayerUpdate(receiveUpdateUserObj.remote, updateUserObj.groupId, updateUserId, updateType);
@@ -51,7 +49,9 @@ class GameTick {
 		}
 	}
 
-	enqueueUpdate(message, offset, remote, token) { // handles back end logic for the actual player updating, and subsequently enqueues the data to be pushed to other players
+	// TODO: sanity checks (verify player movement, throttling/rate limiting, etc);
+	enqueueUpdate(message, offset, remote, token) {
+		// handles back end logic for the actual player updating, and subsequently enqueues the data to be pushed to other players
 
 		// packet structure PLAYER_UPDATE UPDATE_TYPE PLAYER_ID<omitted when receiving an update, only used for sending updates> UPDATE_DATA...
 		let user = this.server.verifyJWT(token);
@@ -59,7 +59,7 @@ class GameTick {
 			const updateType = message.readUInt8(offset);
 			offset++;
 			user = this.server.users.users[user.id];
-			if(this.updateQueue[updateType][user.id]) {
+			if (this.updateQueue[updateType][user.id]) {
 				delete this.updateQueue[updateType][user.id]; // if there are previous updates queued we can safely remove them. (e.g. location updates would stack very fast but we only need the most recent one)
 			}
 			switch (updateType) {
@@ -75,8 +75,10 @@ class GameTick {
 					let username = user.username;
 					const location = new Location(x, y, z);
 					const regionId = location.getPaletteId();
-					if(this.server.users.users[user.id].regionId != regionId) { // we must remove them from their previous region mapping
-						delete this.server.users.regionMap[this.server.users.users[user.id].regionId];
+					if (this.server.users.users[user.id].regionId != regionId) {
+						// we must remove them from their previous region mapping, we can also request all the players in this region here
+						// TODO: request other player's updates for this region ....
+						delete this.server.users.regionMap[this.server.users.users[user.id].regionId][user.id];
 					}
 					this.server.users.users[user.id].regionId = regionId; // assign the new region id and add them to the region map
 					this.server.users.regionMap[regionId] = user.id;
